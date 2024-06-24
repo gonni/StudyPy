547fc6a191fab3d200e9b2e03dd41d351b8ff231
김영곤 (Jeff Kim) <jeff.y.kim@onestorecorp.com>
2024-06-24 19:15:56 +0900

    daily commit

--- Study.py
+++ Study.py
@@ -1,12 +1,19 @@
-def print_list(n):
-    for i in list(range(0,n)):
-        print(i)
+# def print_list(n):
+#     for i in list(range(0,n)):
+#         print(i)
+#
+# print_list(10)
+# print('Hello')
+# print('----------------')
+# ##
+# num = 1
+# while num <= 100:
+#     print(num)
+#     num += 1
 
-print_list(10)
-print('Hello')
-print('----------------')
-##
-num = 1
-while num <= 100:
-    print(num)
-    num += 1
+def multi(m):
+    for n in range(1, 10):
+        print(f'{m} * {n} = {m*n:2d}')
+
+
+multi(3)
